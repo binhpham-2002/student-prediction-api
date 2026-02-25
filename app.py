d5729a2806a643556ff84fbad9c73eb119a3c8ac
Binh Duc Pham <binhpham_2002@Binhs-MacBook-Pro.local>
2026-02-24 17:01:07 -0800

    Update Port

--- app.py
+++ app.py
@@ -26,4 +26,5 @@ def home():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT" 5001))
+    app.run(host="0.0.0.0", port=port)
